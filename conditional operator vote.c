38e837da242e7e677c3ff706a6d518de6528b51e
Abhay Saini <90948162+abhaycodee@users.noreply.github.com>
2021-09-29 17:37:10 +0530

    Add files via upload

---
+++ conditional operator vote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+int i;
+printf("enter the age of person\n");
+scanf("%d",&i);
+i>=18?printf("you are able to vote"):printf("you are not able to vote");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
